13932e7099fe875ce743d813a5615514e564fd14
liuzhushaonian <liuzhushaonian@sina.cn>
2017-06-01 22:24:00 +0800

    first commit

---
+++ app/src/main/java/com/example/legend/lmusic/SerachActivity.java
@@ -0,0 +1,13 @@
+package com.example.legend.lmusic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SerachActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serach);
+    }
+}
